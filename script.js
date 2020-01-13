e24692f83e3a3b6e4917a1825ab714661fa91ec7
Ashton Beaudoin <ashtonbeaudoin2000@hotmail.com>
2020-01-13 15:03:34 -0600

    created the outline of the planner

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-moment().format();  
+var date_time = document.getElementById("date_time");
+date_time.textContent = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+var savedPlans = document.querySelectorAll(".plans")
+for(var i = 0; i < savedPlans.length; i++){
+    var currentPlan = savedPlans[i];
+  var specificTime =  $(currentPlan).attr("id");
+  $(currentPlan).val(localStorage.getItem(specificTime));
+}
+
+
+var event = document.querySelectorAll(".event");
+for(var i=0; i<event.length; i++){
+event[i].addEventListener("click", function(){
+  var input = $(this).siblings(".plans");
+  var time = $(input).attr("id");
+  var plan = $(input).val()
+  localStorage.setItem(time, plan);
+  
+})}
