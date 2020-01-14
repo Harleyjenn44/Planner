4b4a4ae8d37ee9d84b53425df624288c4eb502e2
Ashton Beaudoin <ashtonbeaudoin2000@hotmail.com>
2020-01-13 22:41:28 -0600

    tried to add the time

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 var date_time = document.getElementById("date_time");
-date_time.textContent = moment().format('dddd, '+ 'MMMM Do, YYYY');
+date_time.textContent = moment().format('dddd, ' + 'MMMM Do, YYYY');
 
 var savedPlans = document.querySelectorAll(".plans")
-for(var i = 0; i < savedPlans.length; i++){
-    var currentPlan = savedPlans[i];
-  var specificTime =  $(currentPlan).attr("id");
+for (var i = 0; i < savedPlans.length; i++) {
+  var currentPlan = savedPlans[i];
+  var specificTime = $(currentPlan).attr("id");
   $(currentPlan).val(localStorage.getItem(specificTime));
 }
 
 
 var event = document.querySelectorAll(".event");
-for(var i=0; i<event.length; i++){
-event[i].addEventListener("click", function(){
-  var input = $(this).siblings(".plans");
-  var time = $(input).attr("id");
-  var plan = $(input).val()
-  localStorage.setItem(time, plan);
-  
-})}
+for (var i = 0; i < event.length; i++) {
+  event[i].addEventListener("click", function () {
+    var input = $(this).siblings(".plans");
+    var time = $(input).attr("id");
+    var plan = $(input).val()
+    localStorage.setItem(time, plan);
+
+  })
+}
+var hour = document.querySelectorAll(".plans");
+for (var i = 0; i< hour.length; i++){
+  var currentTime = Date()
+  console.log(currentTime)
+  console.log(hour[i])
+}
+// 1. use jquery to select divs with name plans. 2. gives you an array .each()  also crate var for current time create var that just pulls the hour 3. create if then statement. if 
