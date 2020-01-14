1bf6714803963e0d23f4cf0ee30d7647410d1538
Ashton Beaudoin <ashtonbeaudoin2000@hotmail.com>
2020-01-13 18:27:16 -0600

    started the color change function

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 var date_time = document.getElementById("date_time");
-date_time.textContent = moment().format('MMMM Do YYYY, h:mm:ss a');
+date_time.textContent = moment().format('dddd, '+ 'MMMM Do, YYYY');
+var currentTime = moment();
+
 
 var savedPlans = document.querySelectorAll(".plans")
 for(var i = 0; i < savedPlans.length; i++){
     var currentPlan = savedPlans[i];
   var specificTime =  $(currentPlan).attr("id");
+  specificTime = specificTime.replace(/(am|pm)/, "");
+  
+  if(moment().hours(specificTime)<moment()){
+    console.log("before");
+    $.plans.css("background-color","green")
+  }else if(moment().startOf("hour")< moment().hours(specificTime) < moment().add(1, "hour").startOf(hour)){
+    console.log("during");
+  } else {
+    console.log("after");
+  }
+
   $(currentPlan).val(localStorage.getItem(specificTime));
 }
 
